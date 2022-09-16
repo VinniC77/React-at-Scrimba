23e15575c316a18d049fade47db8dd0eec396d89
VinniC77 <vinniclunc@hotmail.com>
2022-09-15 23:38:58 -0300

    Setting Up and First Line of the course

---
+++ index.js
@@ -0,0 +1,9 @@
+// ReactDOM.render(<h1>Hello, fuckers!</h1>, document.getElementById("root"))
+
+ReactDOM.render(
+<ul>
+    <li>List Item 1</li>
+    <li>List Item 2</li>
+</ul>,
+document.getElementById('root')
+)
