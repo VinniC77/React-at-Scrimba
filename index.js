b3484299af68e3eb38c04559bc2271177aaf78ea
VinniC77 <vinniclunc@hotmail.com>
2022-09-16 23:40:04 -0300

    Criacao de componente

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // ReactDOM.render(<h1>Hello, fuckers!</h1>, document.getElementById("root"))
 
+const MainContent = () => {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
 ReactDOM.render(
+    <div>
 <ul>
     <li>List Item 1</li>
     <li>List Item 2</li>
-</ul>,
+</ul>
+<MainContent />
+</div>,
 document.getElementById('root')
 )
